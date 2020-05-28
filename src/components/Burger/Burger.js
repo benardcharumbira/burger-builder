863022491aff76715db44a8efec785102bab56c7
Benard <benardcharumbira@gmail.com>
2020-05-28 20:45:50 +0200

    Added burger build control

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,26 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+  let tranformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (tranformedIngredients.length === 0) {
+    tranformedIngredients = <p>Please add ingredients</p>;
+  }
+
+  console.log(tranformedIngredients);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {tranformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
