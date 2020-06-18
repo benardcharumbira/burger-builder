24db06418e9e33d4b2356c81d1beed14397a0ad8
Benard <benardcharumbira@gmail.com>
2020-06-18 17:42:49 +0200

    added toobar and logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Auxillary from "../../hoc/Auxillary";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => (
   <Auxillary>
-    <div>Toolbar, Drawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Auxillary>
 );
